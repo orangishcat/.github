18316a21272ba7fa1122d8b10396d8747cd2237b
orangishcat <fdsnail2@gmail.com>
2026-07-04 23:03:47 +0800

    fix: setup astro template

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import svelte from '@astrojs/svelte';
+import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(), mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
