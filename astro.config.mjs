1177e2d9f013796c39fba10fdb24ecf4d765032f
orangishcat <fdsnail2@gmail.com>
2026-07-13 07:17:06 -0700

    fix: remove svelte integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: "https://orangishcat.github.io",
-  integrations: [svelte(), mdx(), sitemap()],
+  integrations: [mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
