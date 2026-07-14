6422fbeb970fde8a4a1734042c3d8e04df7e7148
orangishcat <fdsnail2@gmail.com>
2026-07-13 20:13:34 -0700

    feat: finished projects list

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const projects = [
+  {
+    title: "orangishcat's happy place",
+    description: "(Personal website, the one you're looking at right now)",
+    url: "https://orangishcat.github.io",
+  },
+  {
+    title: "Page Proxy",
+    description: "A Chrome/Firefox extension that streamlines the process of creating userscripts.",
+    url: "https://orangishcat.github.io/page-proxy/"
+  },
+  {
+    title: "Better Clouds",
+    description: "A Minecraft mod that adds volumetric clouds in touch with the aesthetic of vanilla Minecraft",
+    url: "https://modrinth.com/mod/better-clouds",
+    note: "Maintainer"
+  },
+  {
+    title: "Tanks: the Crusades",
+    description: "A cross-platform tank game where the player controls a blue tank with the goal of destroying all enemy tanks to clear a level.",
+    url: "https://modrinth.com/mod/better-clouds",
+    note: "Contributor"
+  },
+];
