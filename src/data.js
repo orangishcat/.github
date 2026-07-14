34a3850bdce16cadc4dfeb1cb19bbb44e5bfcb12
orangishcat <fdsnail2@gmail.com>
2026-07-13 22:59:39 -0700

    feat: add projects list to index page

--- src/data.js
+++ src/data.js
@@ -5,20 +5,20 @@ export const projects = [
     url: "https://orangishcat.github.io",
   },
   {
-    title: "Page Proxy",
-    description: "A Chrome/Firefox extension that streamlines the process of creating userscripts.",
-    url: "https://orangishcat.github.io/page-proxy/"
-  },
-  {
     title: "Better Clouds",
     description: "A Minecraft mod that adds volumetric clouds in touch with the aesthetic of vanilla Minecraft",
     url: "https://modrinth.com/mod/better-clouds",
     note: "Maintainer"
   },
   {
+    title: "Page Proxy",
+    description: "A Chrome/Firefox extension that streamlines the process of creating userscripts.",
+    url: "https://orangishcat.github.io/page-proxy/"
+  },
+  {
     title: "Tanks: the Crusades",
     description: "A cross-platform tank game where the player controls a blue tank with the goal of destroying all enemy tanks to clear a level.",
-    url: "https://modrinth.com/mod/better-clouds",
+    url: "https://github.com/aehmttw/Tanks",
     note: "Contributor"
   },
 ];
